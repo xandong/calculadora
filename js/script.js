a7e68fcdf75e1cf09d7558e61a5075aa2ee69fc2
ventusdigital <94716639+xandong@users.noreply.github.com>
2022-03-23 11:16:12 -0300

    fix

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const display = document.querySelector("#display");
+const displaySecundario = document.querySelector("#display-secundario");
 const displayOperador = document.querySelector("#display-operador");
 
 let [num1, num2, res, operador, isNum1, lastClickIsOperador] = [
@@ -37,28 +38,44 @@ function selectOperator(op) {
     }
   }
   operador = op;
-  displayOperador.innerHTML = `${op}`;
-  // console.log(`Eh num1: ${isNum1}`);
-  // console.log(`Operador: ${operador}`);
+  modifyHTML(0, `${num1.toFixed(2)}`, `${op}`)
 }
 
 function igual() {
-  if (operador == "+") res = num1 + num2;
-  else if (operador == "-") res = num1 - num2;
-  else if (operador == "*") res = num1 * num2;
-  else if (operador == "/") res = num1 / num2;
+  if (operador == "+") res = sum(num1, num2);
+  else if (operador == "-") res = sub(num1, num2);
+  else if (operador == "*") res = mult(num1, num2);
+  else if (operador == "/") res = div(num1, num2);
   if (res == Infinity) {
     reset(0);
     (res = 0), (display.innerHTML = "Error");
   }
-  // console.log(`${num1} ${operador} ${num2} = ${res}`);
+  console.log(`${num1} ${operador} ${num2} = ${res}`);
   reset(res);
-  // console.log(`${num1} ${operador} ${num2} = ${res}`);
+  console.log(`${num1} ${operador} ${num2} = ${res}`);
+}
+
+function sum(num1, num2) {
+  return num1 + num2;
+}
+function sub(num1, num2) {
+  return num1 - num2;
+}
+function mult(num1, num2) {
+  return num1 * num2;
+}
+function div(num1, num2) {
+  return num1 / num2;
 }
 
 function reset(res) {
   invertStateNum();
   [num1, num2, operador] = [res, 0, 0];
-  displayOperador.innerHTML = "";
-  display.innerHTML = `${res.toFixed(2)}`;
+  modifyHTML(`${res.toFixed(2)}`, "", "");
+}
+
+function modifyHTML(args1, args2, args3) {
+  display.innerHTML = args1;
+  displaySecundario.innerHTML = args2;
+  displayOperador.innerHTML = args3;
 }
